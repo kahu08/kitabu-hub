7ca90cb39e56dcfe29ff6df2ea5a2ffe5f96f97e
kahu08 <peterkahurani@gmail.com>
2017-04-22 11:41:07 +0300

    added an invitation model and added an alert to index.hbs

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -6,6 +6,16 @@ export default Ember.Controller.extend({
 
   emailAddress: '',
   /enables invitation button if email is valid/
-  isValid: Ember.computed.match('emailAddress', /^.+@.\..+$/),
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
   isDisabled: Ember.computed.not('isValid')
+
+  actions: {
+    saveInvitation() {
+      alert('saving of the following email is in progress: ${this.get('
+        emailAddress ')}');
+      this.set('responseMessage', 'Thank you! Email has been saved: ${this.get('
+        emailAddress ')}');
+      this.set('emailAddress', '');
+    }
+  }
 });
